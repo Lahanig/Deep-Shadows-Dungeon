edf934b5063b1b65bd459f939f9ca63a0b9f437e
Kaibi <40522793+Lahanig@users.noreply.github.com>
2024-06-14 22:55:27 +0300

    Delete build directory

--- build/index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const Core_1 = require("./Core");
-const Game = new Core_1.Core(new Core_1.Renderer());
-Game.start();
-//# sourceMappingURL=index.js.map
